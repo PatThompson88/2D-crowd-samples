53914f68da8d591f1d8a3619a4ebd891051c08cc
PatThompson88 <patrick.thompson688@gmail.com>
2025-08-17 14:44:15 -0400

    simplified UI controls, logic for random team selection

--- Assets/Crowd/CrowdBehavior.cs
+++ Assets/Crowd/CrowdBehavior.cs
@@ -4,7 +4,6 @@ public enum CrowdState { Normal, Celebrate, Sad };
 public class CrowdBehavior : MonoBehaviour
 {
     public delegate void CrowdEvent(CrowdState state);
-    
     public static CrowdEvent onNewState;
     public SO_CrowdSettings crowdSettings;
     public CrowdState crowdState;
@@ -13,48 +12,50 @@ public class CrowdBehavior : MonoBehaviour
     {
         CrowdNormal();
     }
+
+    // Magic numbers come from Range values in scriptable object SO_CrowdSettings
     public void OnMaxHeightNormalChange(float sliderValue)
     {
-        crowdSettings.maxHeightNormal = sliderValue * 0.5f;
+        crowdSettings.maxHeightNormal = sliderValue * (0.05f - 0.02f) + 0.05f;
     }
     public void OnMaxHeightSadChange(float sliderValue)
     {
-        crowdSettings.maxHeightNormal = sliderValue * 0.5f;
+        crowdSettings.maxHeightNormal = sliderValue * (0.03f - 0.01f) + 0.01f;
     }
     public void OnMaxHeightCelebrateChange(float sliderValue)
     {
-        crowdSettings.maxHeightNormal = sliderValue * 0.5f;
+        crowdSettings.maxHeightNormal = sliderValue * (0.1f - 0.04f) + 0.04f;
     }
     public void OnSpeedNormalChange(float value)
     {
-        crowdSettings.speedNormal = value * 3.3f;
+        crowdSettings.speedNormal = value * (6 - 4) + 4;
     }
     public void OnSpeedCelebrateChange(float value)
     {
-        crowdSettings.speedNormal = value * 0.9f;
+        crowdSettings.speedNormal = value * (3 - 1) + 1;
     }
     public void OnSpeedSadChange(float value)
     {
-        crowdSettings.speedNormal = value * 6;
+        crowdSettings.speedNormal = value * (10 - 6) + 6;
     }
     void OnStateChange()
     {
-        onNewState?.Invoke(crowdState);
-        tmpCrowdState.text = crowdState.ToString();
+        onNewState?.Invoke(crowdSettings.crowdState);
+        tmpCrowdState.text = crowdSettings.crowdState.ToString();
     }
     public void CrowdNormal()
     {
-        crowdState = CrowdState.Normal;
+        crowdSettings.crowdState = CrowdState.Normal;
         OnStateChange();
     }
     public void CrowdCelebrate()
     {
-        crowdState = CrowdState.Celebrate;
+        crowdSettings.crowdState = CrowdState.Celebrate;
         OnStateChange();
     }
     public void CrowdSad()
     {
-        crowdState = CrowdState.Sad;
+        crowdSettings.crowdState = CrowdState.Sad;
         OnStateChange();
     }
 }
--- Assets/Crowd/CrowdMember.cs
+++ Assets/Crowd/CrowdMember.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+public enum Team {home, away};
 public class CrowdMember : MonoBehaviour
 {
     CrowdState fanState;
@@ -10,9 +11,14 @@ public class CrowdMember : MonoBehaviour
     public GameObject imgSad;
     float speedReductionFactor = 2f;
     float maxHeight = 0.14f;
+    Team team;
     void Awake()
     {
         CrowdBehavior.onNewState += HandleNewCrowdState;
+
+        // randomly choose team
+        // crowd should be around 80 - 20 home away
+        team = Random.Range(0, 9) > 1 ? team = Team.home : team = Team.away;
     }
     void Start()
     {
@@ -32,16 +38,16 @@ public class CrowdMember : MonoBehaviour
         {
             case CrowdState.Normal:
                 speedReductionFactor = crowdSettings.speedNormal + Random.Range(-0.4f, 0.4f);
-                maxHeight = crowdSettings.maxHeightNormal + Random.Range(-0.005f, 0.005f); 
+                maxHeight = crowdSettings.maxHeightNormal + Random.Range(-0.008f, 0.008f);
                 imgNormal.SetActive(true);
                 break;
             case CrowdState.Celebrate:
                 speedReductionFactor = crowdSettings.speedCelebrate + Random.Range(-0.15f, 0.15f);
-                maxHeight = crowdSettings.maxHeightCelebrate + Random.Range(-0.005f, 0.005f);
+                maxHeight = crowdSettings.maxHeightCelebrate + Random.Range(-0.01f, 0.01f);
                 imgCelebrate.SetActive(true);
                 break;
             case CrowdState.Sad:
-                speedReductionFactor = crowdSettings.speedSad + Random.Range(-0.3f, 0.3f);
+                speedReductionFactor = crowdSettings.speedSad + Random.Range(-0.6f, 0.6f);
                 maxHeight = crowdSettings.maxHeightCelebrate + Random.Range(-0.005f, 0.005f);
                 imgSad.SetActive(true);
                 break;
